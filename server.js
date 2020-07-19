ccbd3a32d3c8254f72f44fdfd012ed80db04f39d
David Castro Castaneda <dcastrocast@gmail.com>
2020-07-19 02:27:52 -0700

    connection setup

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
 app.listen(PORT, function () {
   console.log('Listening on port: ' + PORT);
 });
